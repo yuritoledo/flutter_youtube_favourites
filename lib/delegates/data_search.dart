1fc03c244d3cf7f0784f16e5aefcdee65732ad99
Yuri Toledo <yuriwtoledo@gmail.com>
2020-04-29 07:34:14 -0300

    DataSearch creation

---
+++ lib/delegates/data_search.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:youtube_favourites/api.dart';
+
+class DataSearch extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {},
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    Future.delayed(Duration.zero).then((_) => close(context, query));
+
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (query.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: suggestions(query),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        final data = snapshot.data;
+
+        return ListView.builder(
+          itemBuilder: (context, index) => ListTile(
+            title: Text(data[index]),
+            leading: Icon(Icons.play_arrow),
+            onTap: () {
+              close(context, data[index]);
+            },
+          ),
+          itemCount: data.length,
+        );
+      },
+    );
+  }
+
+  Future<List> suggestions(String search) async {
+    http.Response response = await http.get(
+        "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json");
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body)[1].map((item) => item[0]).toList();
+    }
+
+    throw new Exception('None @_@');
+  }
+}
