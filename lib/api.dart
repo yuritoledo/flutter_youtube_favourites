ae18f14d685941cad0ef246f7a43f916970900bb
Yuri Toledo <yuriwtoledo@gmail.com>
2020-04-29 07:34:03 -0300

    api creation

---
+++ lib/api.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:youtube_favourites/models/video.dart';
+const API_KEY = 'AIzaSyB3TzqYgtNWFhh_FDY8VaxKZcGJK5A3gLM';
+
+class Api {
+
+  search(String search) async {
+    http.Response resp =  await http.get(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+    );
+
+    return resp;
+  }
+
+  List<Video> decode (http.Response response) {
+    if (response.statusCode == 200) {
+      final decoded = json.decode(response.body);
+      List<Video> list = decoded['items'].map<Video>(
+        (item) => Video.fromJson(item)
+      ).toList();
+
+      return list;
+    }
+
+    throw new Exception('hakuna matata');
+  }
+}
