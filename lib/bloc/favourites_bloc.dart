59fc7a95caaf2ee5408c8786ca5a4db28b518981
Yuri Toledo <yuriwtoledo@gmail.com>
2020-05-14 07:35:24 -0300

    Addind local persistence to fav list

--- lib/bloc/favourites_bloc.dart
+++ lib/bloc/favourites_bloc.dart
@@ -1,12 +1,31 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:youtube_favourites/models/video.dart';
 
 class FavouritesBloc implements BlocBase {
   Map<String, Video> _favourites = {};
 
-  final _favouriteController = StreamController<Map<String, Video>>.broadcast();
+  final _favouriteController =
+      BehaviorSubject<Map<String, Video>>(seedValue: {});
+
+  FavouritesBloc() {
+    _getPersistedData();
+  }
+
+  void _getPersistedData() async {
+    final pref = await SharedPreferences.getInstance();
+    if (pref.getKeys().contains('favourites')) {
+      _favourites = json
+          .decode(pref.getString('favourites'))
+          .map((k, v) => MapEntry(k, Video.fromJson(v)))
+          .cast<String, Video>();
+    }
+    _favouriteController.sink.add(_favourites);
+  }
 
   get favStream => _favouriteController.stream;
 
@@ -18,6 +37,12 @@ class FavouritesBloc implements BlocBase {
     }
 
     _favouriteController.sink.add(_favourites);
+    _persistData();
+  }
+
+  Future<void> _persistData() async {
+    final pref = await SharedPreferences.getInstance();
+    pref.setString('favourites', json.encode(_favourites));
   }
 
   @override
--- lib/models/video.dart
+++ lib/models/video.dart
@@ -7,11 +7,22 @@ class Video {
   Video({this.id, this.title, this.thumb, this.channel});
 
   factory Video.fromJson(Map<String, dynamic> json) {
-    return Video(
-        id: json['id']['videoId'],
-        title: json['snippet']['title'],
-        thumb: json['snippet']['thumbnails']['high']['url'],
-        channel: json['snippet']['channelTitle']
-    );
+    if (json.containsKey('id'))
+      return Video(
+          id: json['id']['videoId'],
+          title: json['snippet']['title'],
+          thumb: json['snippet']['thumbnails']['high']['url'],
+          channel: json['snippet']['channelTitle']);
+    else
+      return Video(
+        id: json['videoId'],
+        channel: json['channel'],
+        thumb: json['thumb'],
+        title: json['title'],
+      );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'videoId': id, 'title': title, 'thumb': thumb, 'channel': channel};
   }
 }
